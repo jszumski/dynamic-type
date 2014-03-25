1fd9adb1aff66e20170427da4452313f8eb43ab2
Richard Das <richard@richarddas.com>
2014-03-25 16:56:55 +0000

    Added support for UIContentSizeCategory and UIFontTextStyle.

--- Dynamic Type/UIFont+AvenirContentSize.m
+++ Dynamic Type/UIFont+AvenirContentSize.m
@@ -10,42 +10,76 @@
 
 @implementation UIFont (AvenirContentSize)
 
-+ (UIFont *)preferredAvenirFontForTextStyle:(NSString *)textStyle {
++ (UIFont *)preferredAvenirFontForTextStyle:(NSString *)textStyle
+{
 	// choose the font size
-	CGFloat fontSize = 16.0;
+	CGFloat fontSize = 16.f;
 	NSString *contentSize = [UIApplication sharedApplication].preferredContentSizeCategory;
-	
-	if ([contentSize isEqualToString:UIContentSizeCategoryExtraSmall]) {
-		fontSize = 12.0;
-		
-	} else if ([contentSize isEqualToString:UIContentSizeCategorySmall]) {
-		fontSize = 14.0;
-		
-	} else if ([contentSize isEqualToString:UIContentSizeCategoryMedium]) {
-		fontSize = 16.0;
-		
-	} else if ([contentSize isEqualToString:UIContentSizeCategoryLarge]) {
-		fontSize = 18.0;
-		
-	} else if ([contentSize isEqualToString:UIContentSizeCategoryExtraLarge]) {
-		fontSize = 20.0;
-		
-	} else if ([contentSize isEqualToString:UIContentSizeCategoryExtraExtraLarge]) {
-		fontSize = 22.0;
-		
-	} else if ([contentSize isEqualToString:UIContentSizeCategoryExtraExtraExtraLarge]) {
-		fontSize = 24.0;
-	}
-	
-	
-	// choose the font weight
+    
+    static NSString *FONT_NAME_REGULAR = @"Avenir-Book";
+    static NSString *FONT_NAME_MEDIUM = @"Avenir-Medium";
+    
+    NSDictionary *fontSizeOffsetDictionary = @{ UIContentSizeCategoryExtraSmall     :   @{ UIFontTextStyleBody : @(-2),
+                                                                                           UIFontTextStyleHeadline : @(-2),
+                                                                                           UIFontTextStyleSubheadline : @(-4),
+                                                                                           UIFontTextStyleCaption1 : @(-5),
+                                                                                           UIFontTextStyleCaption2 : @(-5),
+                                                                                           UIFontTextStyleFootnote : @(-4) },
+                                                
+                                                UIContentSizeCategorySmall          :   @{ UIFontTextStyleBody : @(-1),
+                                                                                           UIFontTextStyleHeadline : @(-1),
+                                                                                           UIFontTextStyleSubheadline : @(-3),
+                                                                                           UIFontTextStyleCaption1 : @(-5),
+                                                                                           UIFontTextStyleCaption2 : @(-5),
+                                                                                           UIFontTextStyleFootnote : @(-4) },
+                                                
+                                                UIContentSizeCategoryMedium         :   @{ UIFontTextStyleBody : @(0),
+                                                                                           UIFontTextStyleHeadline : @(0),
+                                                                                           UIFontTextStyleSubheadline : @(-2),
+                                                                                           UIFontTextStyleCaption1 : @(-5),
+                                                                                           UIFontTextStyleCaption2 : @(-5),
+                                                                                           UIFontTextStyleFootnote : @(-4) },
+                                                
+                                                UIContentSizeCategoryLarge          :   @{ UIFontTextStyleBody : @(1),
+                                                                                           UIFontTextStyleHeadline : @(1),
+                                                                                           UIFontTextStyleSubheadline : @(-1),
+                                                                                           UIFontTextStyleCaption1 : @(-4),
+                                                                                           UIFontTextStyleCaption2 : @(-5),
+                                                                                           UIFontTextStyleFootnote : @(-3) },
+                                                
+                                                UIContentSizeCategoryExtraLarge     :   @{ UIFontTextStyleBody : @(2),
+                                                                                           UIFontTextStyleHeadline : @(2),
+                                                                                           UIFontTextStyleSubheadline : @(0),
+                                                                                           UIFontTextStyleCaption1 : @(-3),
+                                                                                           UIFontTextStyleCaption2 : @(-4),
+                                                                                           UIFontTextStyleFootnote : @(-2) },
+                                                
+                                                UIContentSizeCategoryExtraExtraLarge :  @{ UIFontTextStyleBody : @(3),
+                                                                                           UIFontTextStyleHeadline : @(3),
+                                                                                           UIFontTextStyleSubheadline : @(1),
+                                                                                           UIFontTextStyleCaption1 : @(-2),
+                                                                                           UIFontTextStyleCaption2 : @(-3),
+                                                                                           UIFontTextStyleFootnote : @(-1) },
+                                                
+                                                UIContentSizeCategoryExtraExtraExtraLarge : @{ UIFontTextStyleBody : @(4),
+                                                                                               UIFontTextStyleHeadline : @(4),
+                                                                                               UIFontTextStyleSubheadline : @(2),
+                                                                                               UIFontTextStyleCaption1 : @(-1),
+                                                                                               UIFontTextStyleCaption2 : @(-2),
+                                                                                               UIFontTextStyleFootnote : @(0) } };
+    
+    // adjust the default font size based on what the User has set in Settings
+    CGFloat fontSizeOffset = [fontSizeOffsetDictionary[contentSize][textStyle] doubleValue];
+    fontSize += fontSizeOffset;
+    
+    // choose the font weight
 	if ([textStyle isEqualToString:UIFontTextStyleHeadline] ||
 		[textStyle isEqualToString:UIFontTextStyleSubheadline]) {
-		
-		return [UIFont fontWithName:@"Avenir-Black" size:fontSize];
-		
+        
+		return [UIFont fontWithName:FONT_NAME_MEDIUM size:fontSize];
+        
 	} else {
-		return [UIFont fontWithName:@"Avenir-Medium" size:fontSize];
+		return [UIFont fontWithName:FONT_NAME_REGULAR size:fontSize];
 	}
 }
 
